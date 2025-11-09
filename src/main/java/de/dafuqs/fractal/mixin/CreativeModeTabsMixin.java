9a1cd12a4e6393343ca4d7bed47865c19b100ec1
DaFuqs <dominik.m.schmidt@googlemail.com>
2025-11-09 21:25:31 +0100

    it runssssss!

--- src/main/java/de/dafuqs/fractal/mixin/CreativeModeTabsMixin.java
+++ src/main/java/de/dafuqs/fractal/mixin/CreativeModeTabsMixin.java
@@ -1,13 +1,14 @@
 package de.dafuqs.fractal.mixin;
 
 import de.dafuqs.fractal.api.*;
+import de.dafuqs.fractal.interfaces.*;
 import net.minecraft.world.item.*;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.*;
 
 @Mixin(CreativeModeTabs.class)
-public abstract class CreativeModeTabsMixin {
+public abstract class CreativeModeTabsMixin implements ISubTabLocation {
 	@Inject(at = @At("HEAD"), method = "buildAllTabContents")
 	private static void buildAllTabContents(CreativeModeTab.ItemDisplayParameters parameters, CallbackInfo ci) {
 		CreativeSubTab.SUBTABS.forEach(it -> it.buildContents(parameters));
